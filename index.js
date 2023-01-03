341a3d5161c81af0bec9ace8ab2fb1e1b837445b
Justus <justus.onyancha@moringaschool.com>
2023-01-03 09:19:50 +0300

    add button

--- index.js
+++ index.js
@@ -9,3 +9,40 @@ const doubled = array.map((num) => {
 });
 
 console.log(doubled);
+
+// destructuring assignment
+
+// const first = array[0];
+// const sec = array[1];
+
+//console.log(first, sec);
+
+// lets now destruture the above code
+
+const [first, sec] = array;
+
+console.log(first, sec);
+
+// spread syntax
+console.log("***************************");
+const arr = [1,2,3,4]
+console.log(arr);
+
+function add(x, y){
+    console.log(x + y);
+}
+
+//add(arr[0], arr[1]); // results to 3
+
+add(...arr)
+
+// combined
+
+const combined = [...arr, ...array]
+
+console.log(combined);
+
+// template literals  
+
+const name = "Diane";
+
