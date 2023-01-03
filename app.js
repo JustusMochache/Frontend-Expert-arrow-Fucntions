a9d752af2935e27e06a731dc0ef0aa6aabdf0487
Justus <justus.onyancha@moringaschool.com>
2023-01-03 09:19:22 +0300

    add dom to manipulate event listener

---
+++ app.js
@@ -0,0 +1,7 @@
+const button = document.querySelector('button');
+
+button.addEventListener('click', setBackgroundColor);
+
+function setBackgroundColor(){
+    document.body.style.backgroundColor = '#00334C';
+}
